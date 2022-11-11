63a52f5063f14614faae78db253d1e4b996bf087
AbdulRehmanAtcha <abdulrehman1239091@gmail.com>
2022-11-11 19:10:40 +0500

    Pic Upload

--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJGdAWeut7V4tFSgP5bSyvyi364AOePGw",
+  authDomain: "firstdatabase-7cab1.firebaseapp.com",
+  projectId: "firstdatabase-7cab1",
+  storageBucket: "firstdatabase-7cab1.appspot.com",
+  messagingSenderId: "638833129760",
+  appId: "1:638833129760:web:1ffdf3a0ad555bf10a7f45"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+const auth = getAuth(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
